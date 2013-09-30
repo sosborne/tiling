ee122360148a003d94e21c62115f6addcbee57ac
Steven Osborne <sosborne@iastate.edu>
2013-09-29 19:13:21 -0500

    Commit TETRIS tiling code.

---
+++ tiling.py
@@ -0,0 +1,564 @@
+piece={}
+piece[1] = [[
+             [0,0,0,0,0,0],
+             [0,0,0,0,0,0],
+             [0,0,0,0,0,0],
+             [0,0,1,1,1,1],
+             [0,0,0,0,0,0],
+             [0,0,0,0,0,0]
+             ],
+            [1],
+            {0:2}
+            ]
+piece[2] = [[
+             [0,0,0,0,0,0],
+             [0,0,0,0,0,0],
+             [0,0,0,0,0,0],
+             [0,1,1,1,1,0],
+             [0,0,0,0,0,0],
+             [0,0,0,0,0,0]
+             ],
+            [2],
+            {0:3}
+            ]
+piece[3] = [[
+             [0,0,0,0,0,0],
+             [0,0,0,0,0,0],
+             [0,0,0,0,0,0],
+             [1,1,1,1,0,0],
+             [0,0,0,0,0,0],
+             [0,0,0,0,0,0]
+             ],
+            [3],
+            {0:0}
+            ]
+piece[4] = [[
+             [0,0,0,0,0,0],
+             [0,0,0,0,0,0],
+             [0,0,0,0,0,0],
+             [0,0,1,1,0,0],
+             [0,1,1,0,0,0],
+             [0,0,0,0,0,0]
+             ],
+            [4],
+            {0:0}
+            ]
+piece[5] = [[
+             [0,0,0,0,0,0],
+             [0,0,0,0,0,0],
+             [0,0,1,0,0,0],
+             [0,0,1,1,0,0],
+             [0,0,0,1,0,0],
+             [0,0,0,0,0,0]
+             ],
+            [5,0],
+            {-1:0,0:0}
+            ]
+piece[6] = [[
+             [0,0,0,0,0,0],
+             [0,0,0,0,0,0],
+             [0,0,0,1,1,0],
+             [0,0,1,1,0,0],
+             [0,0,0,0,0,0],
+             [0,0,0,0,0,0]
+             ],
+            [6,0],
+            {0:0,1:4}
+            ]
+piece[7] = [[
+             [0,0,0,0,0,0],
+             [0,0,0,0,0,0],
+             [0,1,1,0,0,0],
+             [0,0,1,1,0,0],
+             [0,0,0,0,0,0],
+             [0,0,0,0,0,0]
+             ],
+            [7,0],
+            {0:0}
+            ]
+piece[8] = [[
+             [0,0,0,0,0,0],
+             [0,0,0,0,0,0],
+             [0,0,0,1,0,0],
+             [0,0,1,1,0,0],
+             [0,0,1,0,0,0],
+             [0,0,0,0,0,0]
+             ],
+            [8,0],
+            {0:0,1:0}
+            ]
+piece[9] = [[
+             [0,0,0,0,0,0],
+             [0,0,0,0,0,0],
+             [0,0,0,0,0,0],
+             [0,0,1,1,0,0],
+             [0,0,0,1,1,0],
+             [0,0,0,0,0,0]
+             ],
+            [9],
+            {-1:7,0:0}
+            ]
+piece[10] = [[
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,1,0,0,0],
+              [0,0,1,1,1,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0]
+              ],
+             [10,0],
+             {0:11}
+             ]
+piece[11] = [[
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,1,0,0,0,0],
+              [0,1,1,1,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0]
+              ],
+             [11],
+             {0:0}
+             ]
+piece[12] = [[
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,1,0],
+              [0,0,1,1,1,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0]
+              ],
+             [12],
+             {0:13,1:0}
+             ]
+piece[13] = [[
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,1,0,0],
+              [0,1,1,1,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0]
+              ],
+             [13,0],
+             {0:0,1:0}
+             ]
+piece[14] = [[
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,1,1,1,0],
+              [0,0,1,0,0,0],
+              [0,0,0,0,0,0]
+              ],
+             [14],
+             {0:15}
+             ]
+piece[15] = [[
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,1,1,1,0,0],
+              [0,1,0,0,0,0],
+              [0,0,0,0,0,0]
+              ],
+             [15],
+             {0:0}
+             ]
+piece[16] = [[
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,1,1,1,0],
+              [0,0,0,0,1,0],
+              [0,0,0,0,0,0]
+              ],
+             [16],
+             {-1:0,0:17}
+             ]
+piece[17] = [[
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,1,1,1,0,0],
+              [0,0,0,1,0,0],
+              [0,0,0,0,0,0]
+              ],
+             [17],
+             {-1:0,0:0}
+             ]
+piece[18] = [[
+              [0,0,0,0,0,0],
+              [0,0,1,0,0,0],
+              [0,0,1,0,0,0],
+              [0,0,1,1,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0]
+              ],
+             [18,0,0],
+             {0:0}
+             ]
+piece[19] = [[
+              [0,0,0,0,0,0],
+              [0,0,0,1,0,0],
+              [0,0,0,1,0,0],
+              [0,0,1,1,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0]
+              ],
+             [19,0,0],
+             {0:0,1:0,2:0}
+             ]
+piece[20] = [[
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,1,1,0,0],
+              [0,0,1,0,0,0],
+              [0,0,1,0,0,0]
+              ],
+             [20],
+             {0:0}
+             ]
+piece[21] = [[
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,1,1,0,0],
+              [0,0,0,1,0,0],
+              [0,0,0,1,0,0]
+              ],
+             [21],
+             {-2:0,-1:0,0:0}
+             ]
+piece[22] = [[
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,1,0,0,0],
+              [0,1,1,1,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0]
+              ],
+             [22,0],
+             {0:0}
+             ]
+piece[23] = [[
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,1,0,0],
+              [0,0,1,1,1,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0]
+              ],
+             [23,0],
+             {0:22,1:0}
+             ]
+piece[24] = [[
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,1,0,0,0],
+              [0,0,1,1,0,0],
+              [0,0,1,0,0,0],
+              [0,0,0,0,0,0]
+              ],
+             [24,0],
+             {0:0}
+             ]
+piece[25] = [[
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,1,1,1,0],
+              [0,0,0,1,0,0],
+              [0,0,0,0,0,0]
+              ],
+             [25],
+             {-1:0,0:26}
+             ]
+piece[26] = [[
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,1,1,1,0,0],
+              [0,0,1,0,0,0],
+              [0,0,0,0,0,0]
+              ],
+             [26],
+             {0:0}
+             ]
+piece[27] = [[
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,1,0,0],
+              [0,0,1,1,0,0],
+              [0,0,0,1,0,0],
+              [0,0,0,0,0,0]
+              ],
+             [27,0],
+             {-1:0,0:0,1:0}
+             ]
+piece[28] = [[
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,1,1,0,0],
+              [0,0,1,1,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0]
+              ],
+             [28,28],
+             {0:0,1:0}
+             ]
+amnesia = [
+           piece[1],
+           piece[5],
+           piece[6],
+           piece[8],
+           piece[9],
+           piece[10],
+           piece[12],
+           piece[14],
+           piece[16],
+           piece[18],
+           piece[19],
+           piece[20],
+           piece[21],
+           piece[23],
+           piece[24],
+           piece[25],
+           piece[27],
+           piece[28]
+           ]
+
+def piece_to_grid(piece,pointer,n):
+    new_piece=copy(piece)
+    new_piece.reverse()
+    grid = []
+    if pointer<2:
+        for i in range(0,2-pointer):
+            if 1 in new_piece[i]:
+                return 'Problem'
+    if n-1-pointer<3:
+        for i in range(0,3-n+1+pointer):
+            if 1 in new_piece[5-i]:
+                return 'Problem'
+    
+    if pointer<2:
+        for i in range(2-pointer,min(6,n+2-pointer)):
+            grid.append(new_piece[i])
+        for i in range(len(grid),n):
+            grid.append([0,0,0,0,0,0])
+    elif 2 <= pointer <= n-4:
+        for i in range(0,pointer-2):
+            grid.append([0,0,0,0,0,0])
+        for i in range(0,6):
+            grid.append(new_piece[i])
+        for i in range(len(grid),n):
+            grid.append([0,0,0,0,0,0])
+    elif pointer>n-4:
+        for i in range(0,pointer-2):
+            grid.append([0,0,0,0,0,0])
+        for i in range(0,n-pointer+2):
+            grid.append(new_piece[i])
+    grid.reverse()
+    return grid
+
+def try_pair(grid1,grid2):
+    (n,m) = (len(grid1),len(grid1[0]))
+    new_grid = []
+    for i in range(0,n):
+        new_grid.append([])
+        for j in range(0,m):
+            sum = grid1[i][j] + grid2[i][j]
+            if sum == 2:
+                return 'Problem'
+            else:
+                new_grid[i].append(sum)
+    return new_grid
+
+def left_count(grid):
+    count = 0
+    for row in grid:
+        for i in range(0,3):
+            count+=row[i]
+    return count
+
+def build_crossings(grid,pointer,keeping_track,counter):
+    global CROSSINGS
+    n = len(grid)
+    if pointer == n:
+        if tuple(keeping_track) not in CROSSINGS:
+            if n%4==0:
+                if left_count(grid)%4==0:
+                    CROSSINGS[tuple(keeping_track)] = [counter[0],grid]
+                    counter[0]+=1
+            elif n%2==0:
+                if left_count(grid)%2==0:
+                    CROSSINGS[tuple(keeping_track)] = [counter[0],grid]
+                    counter[0]+=1
+            else:
+                CROSSINGS[tuple(keeping_track)] = [counter[0],grid]
+                counter[0]+=1
+    
+    else:
+        new_grid = copy(grid)
+        new_keeping_track = copy(keeping_track)
+        new_keeping_track.append(0)
+        build_crossings(new_grid,pointer+1,new_keeping_track,counter)
+        for i in range(1,29):
+            new_grid = copy(grid)
+            new_keeping_track = copy(keeping_track)
+            a_piece=copy(piece[i][0])
+            extension=copy(piece[i][1])
+            grid_piece = piece_to_grid(a_piece,pointer,n)
+            if grid_piece != 'Problem':
+                combined_grid = try_pair(grid_piece,new_grid)
+                if combined_grid != 'Problem':
+                    new_keeping_track.extend(extension)
+                    build_crossings(combined_grid,pointer+len(extension),new_keeping_track,counter)
+
+def shift_grid(grid):
+    (n,m) = (len(grid),len(grid[0]))
+    new_grid=[]
+    for i in range(0,n):
+        new_grid.append([])
+        for j in range(1,m):
+            new_grid[i].append(grid[i][j])
+        new_grid[i].append(0)
+    return new_grid
+
+def forced_mate(crossing,grid):
+    mate = [-1]*len(crossing)
+    for i in range(0,len(crossing)):
+        the_key=crossing[i]
+        if 0 < the_key < 28:
+            to_force = piece[the_key][2]
+            for j in to_force.keys():
+                mate[i+j]=to_force[j]
+        elif the_key==28:
+            mate[i]=0
+    return mate
+
+def fill_mate(original_crossing,mate_so_far,grid,neighbors):
+    if -1 not in mate_so_far:
+        mate_so_far = tuple(mate_so_far)
+        if mate_so_far in CROSSINGS and mate_so_far != tuple([0]*len(mate_so_far)):
+            if no_holes(grid):
+                neighbors.append(CROSSINGS[mate_so_far][0])
+    else:
+        pointer = mate_so_far.index(-1)
+        new_grid = copy(grid)
+        new_mate_so_far = copy(mate_so_far)
+        new_mate_so_far[pointer]=0
+        fill_mate(original_crossing,new_mate_so_far,new_grid,neighbors)
+        
+        for i in range(0,18):
+            new_grid = copy(grid)
+            new_mate_so_far = copy(mate_so_far)
+            a_piece=copy(amnesia[i][0])
+            extension=copy(amnesia[i][1])
+            grid_piece = piece_to_grid(a_piece,pointer,n)
+            if grid_piece != 'Problem':
+                combined_grid = try_pair(grid_piece,new_grid)
+                if combined_grid != 'Problem':
+                    Problem = False
+                    for j in range(0,len(extension)):
+                        if new_mate_so_far[pointer+j] in [-1,extension[j]]:
+                            new_mate_so_far[pointer+j] = extension[j]
+                        else:
+                            Problem = True
+                    if not Problem:
+                        fill_mate(original_crossing,new_mate_so_far,combined_grid,neighbors)
+
+def no_holes(grid):
+    n = len(grid)
+    no_holes=True
+    i = 0
+    while i < n and no_holes==True:
+        if grid[i][2]==0:
+            if i <= n-4 and grid[i+1][2]==grid[i+2][2]==grid[i+3][2]==0:
+                i+=4
+            else:
+                no_holes=False
+        else:
+            i+=1
+    return no_holes
+
+def zero_neighbor(grid):
+    n = len(grid)
+    left_grid = []
+    right_grid= []
+    for i in range(0,n):
+        left_grid.append([0,1,0,0,0,0])
+        right_grid.append([0,0,0,0,1,0])
+    
+    new_grid=try_pair(left_grid,grid)
+    if new_grid != 'Problem' and no_holes(new_grid):
+        left_ok = True
+    else:
+        left_ok = False
+    
+    new_grid=try_pair(grid,right_grid)
+    if new_grid != 'Problem' and no_holes(shift_grid(new_grid)):
+        right_ok = True
+    else:
+        right_ok = False
+    
+    return(left_ok,right_ok)
+
+def construct_sparse_matrix(crossings,neighbors):
+    sparse_matrix={}
+    for i in crossings:
+        sparse_matrix[i]=tuple(neighbors[i])
+    return sparse_matrix
+
+def sparse_power(sparse_matrix,N):
+    M=[0]*len(sparse_matrix)
+    M[0]=1
+    for q in range(N):
+        new_M=[0]*len(sparse_matrix)
+        for i in sparse_matrix:
+            for t in sparse_matrix[i]:
+                new_M[i]+=M[t]
+        M=new_M
+        print str(q+1)+"\t"+str(M[0])
+
+
+def show_tilings(n):
+    print 'Working on height ' + str(n)
+    base_grid=[]
+    for i in range(0,n):
+        base_grid.append([0,0,0,0,0,0])
+    base_crossing=tuple([0]*n)
+    global CROSSINGS
+    CROSSINGS={}
+    build_crossings(base_grid,0,[],[0])
+    
+    print 'Number of crossings: ' + str(len(CROSSINGS.keys()))
+    reordered_crossings={}
+    for crossing in CROSSINGS:
+        reordered_crossings[CROSSINGS[crossing][0]]=[crossing,CROSSINGS[crossing][1]]
+    
+    NEIGHBORS={}
+    NEIGHBORS[0]=[]
+    for crossing in CROSSINGS:
+        n = len(crossing)
+        crossing_index = CROSSINGS[crossing][0]
+        if crossing_index != 0:
+            new_grid = shift_grid(CROSSINGS[crossing][1])
+            neighbors=[]
+            fill_mate(crossing,forced_mate(crossing,new_grid),new_grid,neighbors)
+            NEIGHBORS[crossing_index]=neighbors
+            
+            (left_zero,right_zero) = zero_neighbor(CROSSINGS[crossing][1])
+            if left_zero:
+                NEIGHBORS[0].append(crossing_index)
+            if right_zero:
+                NEIGHBORS[crossing_index].append(0)
+    
+    
+    (left_zero,right_zero) = zero_neighbor(base_grid)
+    if left_zero:
+        NEIGHBORS[0].append(0)
+    
+    print 'length \t tilings'
+    M = construct_sparse_matrix(reordered_crossings,NEIGHBORS)
+    sparse_power(M,10)
